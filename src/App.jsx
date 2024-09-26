c46b85c69f3e28e0fe66050daeb8262667dbfef9
Zmito26dev <zmito26dev@gmail.com>
2024-09-26 17:45:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+function App() {
+  const WidthLink = ({name, url, icon}) => {
+    return (
+      <a className="width-link" href={url} draggable="false">
+        <p>{name}</p>
+      </a>
+    )
+  }
+
+  return (
+    <>
+      <div className="main">
+        <div className="app">
+          <div className="header">
+            <img className="logo" src="src/assets/logo.png" alt="" draggable="false"/>
+            <h1 className="username">@Zmito26</h1>
+          </div>
+          <div className="big-links">
+            <WidthLink name="Twitch" url="https://twitch.tv/zmito26"/>
+            <WidthLink name="Youtube" url="https://www.youtube.com/@zmito26_"/>
+            <WidthLink name="Instagram" url="https://www.instagram.com/zmito26"/>
+            <WidthLink name="Twitter [X]" url="https://x.com/zmito26"/>
+            <WidthLink name="Tiktok" url="https://tiktok.com/zmito26"/>
+          </div>
+        </div> 
+      </div>
+    </>
+  )
+}
+
+export default App
